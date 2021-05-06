656e88628991e388b223de10f1da72733c9d258d
Bruno Souza <bruno1994deilson@gmail.com>
2021-05-06 16:33:26 -0300

    Layers

---
+++ backend/src/main/java/com/desvsuperior/dsvendas/control/SellerController.java
@@ -0,0 +1,26 @@
+package com.desvsuperior.dsvendas.control;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.desvsuperior.dsvendas.dto.SellerDTO;
+import com.desvsuperior.dsvendas.service.SellerService;
+
+@RestController
+@RequestMapping(value = "/sellers")
+public class SellerController {
+
+	@Autowired
+	private SellerService service;
+	
+	@GetMapping
+	public ResponseEntity<List<SellerDTO>> findAll(){
+		List<SellerDTO> list = service.findAll();
+		return ResponseEntity.ok(list);
+	}
+}
