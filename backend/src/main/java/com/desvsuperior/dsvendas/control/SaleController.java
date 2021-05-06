7d45295f77858272b12ee9fc8f8af92fcd6ec209
Bruno Souza <bruno1994deilson@gmail.com>
2021-05-06 17:53:02 -0300

    Pagination

---
+++ backend/src/main/java/com/desvsuperior/dsvendas/control/SaleController.java
@@ -0,0 +1,26 @@
+package com.desvsuperior.dsvendas.control;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.desvsuperior.dsvendas.dto.SaleDTO;
+import com.desvsuperior.dsvendas.service.SaleService;
+
+@RestController
+@RequestMapping(value = "/sales")
+public class SaleController {
+
+	@Autowired
+	private SaleService service;
+	
+	@GetMapping
+	public ResponseEntity<Page<SaleDTO>> findAll(Pageable pageable){
+		Page<SaleDTO> list = service.findAll(pageable);
+		return ResponseEntity.ok(list);
+	}
+}
---
+++ backend/src/main/java/com/desvsuperior/dsvendas/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.desvsuperior.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.desvsuperior.dsvendas.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
